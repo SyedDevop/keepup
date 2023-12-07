71e91a700f9943aa4e64b1321b8a968fe4a9e2d7
syed Devop <syeds.devops007@gmail.com>
2023-12-07 21:03:30 +0530

    feat: add fonts and removed example code.

--- src/App.tsx
+++ src/App.tsx
@@ -1,69 +1,9 @@
-import { useEffect, useState } from "react";
-import reactLogo from "./assets/react.svg";
-import { invoke } from "@tauri-apps/api/tauri";
 import "./App.css";
-import { createClient } from "@libsql/client/web";
 
 function App() {
-  const [greetMsg, setGreetMsg] = useState("");
-  const [name, setName] = useState("");
-
-  useEffect(() => {
-    // Synchronize the embedded replica with the remote database
-    (async () => {
-      const envValue: string = await invoke("get_env");
-      const env = JSON.parse(envValue);
-      const client = createClient({
-        url: env["sync_url"],
-        syncUrl: "file:./test.db",
-        authToken: env["auth_token"],
-      });
-      await client.sync();
-
-      console.log(await client.execute("select * from keepup"));
-    })();
-  }, []);
-
-  async function greet() {
-    // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-    setGreetMsg(await invoke("greet", { name }));
-  }
-
   return (
     <div className="container">
       <h1>Welcome to Tauri!</h1>
-
-      <div className="row">
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo vite" alt="Vite logo" />
-        </a>
-        <a href="https://tauri.app" target="_blank">
-          <img src="/tauri.svg" className="logo tauri" alt="Tauri logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-
-      <p>Click on the Tauri, Vite, and React logos to learn more.</p>
-
-      <form
-        className="row"
-        onSubmit={(e) => {
-          e.preventDefault();
-          greet();
-        }}
-      >
-        <input
-          id="greet-input"
-          onChange={(e) => setName(e.currentTarget.value)}
-          placeholder="Enter a name..."
-        />
-        <button type="submit">Greet</button>
-      </form>
-
-      <p>{greetMsg}</p>
-      <p>Hello from frount</p>
     </div>
   );
 }
